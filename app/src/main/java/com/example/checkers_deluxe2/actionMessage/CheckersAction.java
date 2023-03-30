505a5cd55e852fec809eb3ab1690939ef8e803c7
Ash <ashtonchar22@gmail.com>
2023-03-30 10:58:54 -0700

    added CheckersAction class

---
+++ app/src/main/java/com/example/checkers_deluxe2/actionMessage/CheckersAction.java
@@ -0,0 +1,16 @@
+package com.example.checkers_deluxe2.actionMessage;
+
+import com.example.GameFramework.actionMessage.GameAction;
+import com.example.GameFramework.players.GamePlayer;
+
+public class CheckersAction extends GameAction {
+
+    /**
+     * The inherited constructor from AnimationSurface class
+     *
+     * @param player the player who created the action
+     */
+    public CheckersAction(GamePlayer player) {
+        super(player);
+    }//ctor
+}
