fd08d9fd9de4f093b46f44adf031f046a88d33a6
Matt <mddavid1302@gmail.com>
2023-04-04 22:26:08 -0700

    deleted tests

---
+++ app/src/main/java/com/example/checkers_deluxe2/actionMessage/CheckersTapPiece.java
@@ -0,0 +1,17 @@
+package com.example.checkers_deluxe2.actionMessage;
+
+import com.example.GameFramework.actionMessage.GameAction;
+import com.example.GameFramework.players.GamePlayer;
+
+public class CheckersTapPiece extends GameAction {
+
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public CheckersTapPiece(GamePlayer player) {
+        super(player);
+    }
+
+}
